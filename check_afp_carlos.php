6ad7e5b91e9188af5b7ed801a6876ad36748ac9b
Ricardo Sanchez <ricardosanchezmarquez@gmail.com>
2026-08-24 17:36:49 -0500

    chore: limpiar archivos de prueba y debug que se colaron al commit anterior

--- check_afp_carlos.php
+++
@@ -1,6 +0,0 @@
-<?php
-$l = \App\Models\PlanillaLiquidacion::where('dni', '45123456')->where('periodo', '2026-08')->first();
-print_r($l->only([
-    'sistema_pensiones', 'remuneracion_bruta', 'descuento_pension',
-    'afp_comision_flujo', 'afp_prima_seguro', 'afp_aporte_obligatorio', 'porcentaje_pension',
-]));
